b3e784aaac57ea2e69bd58c726feb6fb132995c7
ibrahim Bozkurt <ibrahim_bozkurt@hotmail.com.tr>
2022-10-19 22:40:07 +0300

    first commit

---
+++ datatypes/datatypes/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        byte artiByteSayi = 127;
+        byte eksiByteSayi = -128;
+
+        int artiİntSayi = 2147483647;
+        int eksiSayi = -2147483648;
+
+        char karakter = 'A';
+
+        boolean dogruMu = false;
+
+
+
+    }
+}
---
+++ switchDemo/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        char grade = 'H';
+
+        switch (grade) {
+            case 'A':
+                System.out.println("Mükemmel: Geçtiniz");
+                break;
+            case 'B':
+                System.out.println("Çok Güzel: Geçtiniz.");
+                break;
+            case 'C':
+                System.out.println("İyi Geçtiniz");
+                break;
+            case 'D':
+                System.out.println("Fenal Değil: Geçtiniz");
+                break;
+            case 'F':
+                System.out.println("Maalesef: Kaldınız.");
+                break;
+            default:
+                System.out.println("Sistemde geçerli olmayan bir not girdiniz.");
+        }
+    }
+}
